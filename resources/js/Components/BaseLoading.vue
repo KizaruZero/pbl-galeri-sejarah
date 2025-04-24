843b98c2e7ac59b360a325c538ff1db22060d602
agoyeeee <prayogaputra70@gmail.com>
2025-04-24 22:40:14 +0700

    FEAT : Loading Global

---
+++ resources/js/Components/BaseLoading.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/50" v-if="show">
+        <looping-rhombuses-spinner :animation-duration="2500" :rhombus-size="15" color="#ffffff" />
+    </div>
+</template>
+
+<script setup>
+    import {
+        LoopingRhombusesSpinner
+    } from 'epic-spinners'
+    import {
+        ref,
+        onMounted
+    } from 'vue'
+    import {
+        router
+    } from '@inertiajs/vue3'
+
+    const show = ref(false)
+
+    onMounted(() => {
+        router.on('start', () => show.value = true)
+        router.on('finish', () => show.value = false)
+    })
+
+</script>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from '@/Components-landing/Navbar.vue'
 import Footer from '@/Components-landing/Footer.vue'
+import BaseLoading from '@/Components/BaseLoading.vue'
 </script>
 
 <template>
+    <BaseLoading /> 
   <Navbar />
   <main>
     <slot></slot>
