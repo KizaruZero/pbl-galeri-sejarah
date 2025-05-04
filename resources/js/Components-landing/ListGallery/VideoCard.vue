447d2011a7bdaf9e693ef77fc2082825c02fde99
agoyeeee <prayogaputra70@gmail.com>
2025-05-04 22:01:22 +0700

    FIX : Video Gallery

--- resources/js/Components-landing/ListGallery/VideoCard.vue
+++ resources/js/Components-landing/ListGallery/VideoCard.vue
@@ -1,16 +1,16 @@
 <template>
-  <article class="overflow-hidden bg-black rounded-xl shadow-md transition-all duration-300 
+  <article class="overflow-hidden bg-black rounded-xl shadow-md transition-all duration-300
            hover:scale-[1.02] hover:shadow-lg cursor-pointer"
     @click="$emit('click')">
-    
+
     <!-- Image Container -->
     <div class="card relative w-full h-[220px] sm:h-[240px] md:h-[260px] lg:h-[300px]">
-      <img :src="video_url" :alt="title"
+      <img :src="thumbnailUrl" :alt="title"
         class="w-full h-full object-cover rounded-t-xl"
         @error="handleVideoError"
       />
     </div>
-    
+
     <!-- Rest of your card content -->
     <div class="p-5 text-center">
       <h3 class="font-semibold text-white leading-snug mb-2" :class="titleClass">
@@ -30,7 +30,7 @@
 import { computed } from "vue";
 
 const props = defineProps({
-        video_url: {
+        thumbnailUrl: {
             type: String,
             required: true,
         },
