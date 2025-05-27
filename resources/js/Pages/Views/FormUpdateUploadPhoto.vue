22e0a4e5e25b9df008e07b01d5977348aa579403
RaihanIan <mraihanyan@student.uns.ac.id>
2025-05-27 16:51:00 +0700

    fix : ketinggalan lagi mwehe

--- resources/js/Pages/Views/FormUpdateUploadPhoto.vue
+++ resources/js/Pages/Views/FormUpdateUploadPhoto.vue
@@ -1,8 +1,8 @@
 <template>
     <MainLayout>
-        <div class="mt-14 mx-auto p-6 bg-[#0d0d0d] max-w-full">
+        <div class="mx-auto p-6 bg-[#0d0d0d] max-w-full">
             <button @click="visitBacktoProfile"
-                class="mb-6 mt-14 flex items-center text-gray-400 hover:text-blue-300 transition-colors">
+                class="mb-6 mt-4 flex items-center text-gray-400 hover:text-blue-300 transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd"
                         d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
