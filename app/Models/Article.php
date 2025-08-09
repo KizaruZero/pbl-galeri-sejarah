2b77526b6650ff73bf688b303b77bca12b54f08a
KizaruZero <ardyapusaka@gmail.com>
2025-08-09 15:54:37 +0700

    REMOVE: Commented out booted method for article model to clean up code

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -67,12 +67,6 @@ class Article extends Model
         $this->save();
     }
 
-    // protected static function booted()
-    // {
-    //     static::created(function ($article) {
-    //         app()->call([Article::class, 'getHourlyUploadedContent']);
-    //     });
-    // }
 
 
 }
